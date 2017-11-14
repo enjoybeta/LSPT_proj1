6e431bd229faf4f7c35ae66aa14333b00d598742
enjoybeta <goujiashun@gmail.com>
2017-11-13 22:59:58 -0500

    test Github

---
+++ src/FirstTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+
+	@Test
+	void test() {
+		//assert(0==0);
+		//fail("Not yet implemented");
+	}
+
+}
