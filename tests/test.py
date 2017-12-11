3cf7adc97673ebc817aca6112d43cb04b4b32f4e
enjoybeta <goujiashun@gmail.com>
2017-12-11 17:30:24 -0500

    update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,40 +1,49 @@
+#example to run
+#python3 test.py "128.113.126.17:8080"
+
 import sys
 import glob, os
 import json
 import requests
 
 def main(argv):
-    #getHealth()
-    #getStopWords()
-    setTokens()
-    getTokens()
+    print("num of input argument:"+str(len(argv)))
+    if len(argv) == 1:
+        ip = "127.0.0.1:8080"
+    else:
+        ip = argv[1]
+    print("ip:"+ip)
+    getHealth(ip)
+    getStopWords(ip)
+    #setTokens(ip)
+    #getTokens(ip)
 
-def getHealth():
-    r = requests.get("http://127.0.0.1:8080/health")
+def getHealth(ip):
+    r = requests.get("http://"+ip+"/health")
     print(r.text)
 
-def getStopWords():
-    r = requests.get("http://127.0.0.1:8080/stopWords")
+def getStopWords(ip):
+    r = requests.get("http://"+ip+"/stopWords")
     print(r.text)
 
-def setTokens():
+def setTokens(ip):
     for fileName in glob.iglob('text transformation/*.json'):
         with open(fileName, 'r') as myfile:
             dataStr=myfile.read().replace('\n', '').replace('\t', '')
         #print(dataStr)
-        print("Sending setToken as POST request to 127.0.0.1:8080")
+        print("Sending setToken as POST request to "+ip)
         headers = {'Accept' : 'application/json', 'Content-Type' : 'text/plain'}
-        r = requests.post("http://127.0.0.1:8080/setToken", data=dataStr,headers=headers)
+        r = requests.post("http://"+ip+"/setToken", data=dataStr,headers=headers)
         print(r.text)
         
-def getTokens():
+def getTokens(ip):
     for fileName in glob.iglob('ranking/*.json'):
         with open(fileName, 'r') as myfile:
             dataStr=myfile.read().replace('\n', '').replace('\t', '')
         #print(dataStr)
-        print("Sending getToken as POST request to 127.0.0.1:8080")
+        print("Sending getToken as POST request to "+ip)
         headers = {'Accept' : 'application/json', 'Content-Type' : 'text/plain'}
-        r = requests.post("http://127.0.0.1:8080/getToken", data=dataStr,headers=headers)
+        r = requests.post("http://"+ip+"/getToken", data=dataStr,headers=headers)
         print(r.text)
         
 if __name__ == "__main__":
